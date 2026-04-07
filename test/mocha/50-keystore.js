727f6e38a0e3ad46d0e0875dc352e24401bd20d1
Dave Longley <dlongley@digitalbazaar.com>
2026-04-07 14:07:56 -0400

    Add `KmsModule` class and update main module API.

--- test/mocha/50-keystore.js
+++ test/mocha/50-keystore.js
@@ -15,22 +15,38 @@ import {_createKeyRecordCipher} from '@bedrock/kms-module-core';
 'u' + Buffer.concat([Buffer.from([0xa2, 0x01]), Buffer.from(crypto.getRandomValues(new Uint8Array(32)))]).toString('base64url')
 */
 /* eslint-enable */
-const keyRecordEncryption = [
+const testParameters = [
   {
-    title: 'w/no wrapping',
-    kek: null
+    title: 'no encryption',
+    encryptConfig: {currentKekId: null},
+    shouldEncrypt: false
   },
   {
-    title: 'w/aes256 wrapping',
-    kek: {
-      id: 'urn:test:aes256',
-      secretKeyMultibase: 'uogH3ERq9FRYOV8IuUiD2gKZs_qN6SLU-6RtbBUfzqQwGdg'
-    }
+    title: 'w/aes256 encryption w/json encoding',
+    encryptConfig: {
+      encoding: 'cbor',
+      keks: [{
+        id: 'urn:test:aes256',
+        secretKeyMultibase: 'uogH3ERq9FRYOV8IuUiD2gKZs_qN6SLU-6RtbBUfzqQwGdg'
+      }]
+    },
+    shouldEncrypt: true
+  },
+  {
+    title: 'w/aes256 encryption w/cbor encoding',
+    encryptConfig: {
+      encoding: 'json',
+      keks: [{
+        id: 'urn:test:aes256',
+        secretKeyMultibase: 'uogH3ERq9FRYOV8IuUiD2gKZs_qN6SLU-6RtbBUfzqQwGdg'
+      }]
+    },
+    shouldEncrypt: true
   }
 ];
 
-for(const encryptConfig of keyRecordEncryption) {
-  describe(`keystore ${encryptConfig.title}`, () => {
+for(const {title, encryptConfig, shouldEncrypt} of testParameters) {
+  describe(`keystore ${title}`, () => {
     const moduleConfig = bedrock.config['ssm-mongodb'];
     const oldConfigValue = moduleConfig.keyRecordEncryption;
     before(async () => {
