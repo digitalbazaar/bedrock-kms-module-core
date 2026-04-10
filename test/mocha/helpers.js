8ae4b07c0e92a7f7803ba58ec7e352722d735ab7
Dave Longley <dlongley@digitalbazaar.com>
2026-04-09 22:55:12 -0400

    Update tests.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -1,6 +1,14 @@
 /*!
  * Copyright (c) 2021-2026 Digital Bazaar, Inc.
  */
+import * as database from '@bedrock/mongodb';
+
+export const MOCKS = {};
+
+export async function clearCollection({collectionName} = {}) {
+  await database.collections[collectionName].deleteMany({});
+}
+
 export function localId({id}) {
   const idx = id.lastIndexOf('/');
   return id.substring(0, idx);
